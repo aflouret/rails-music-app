a059591b67ef69fe6315631c742b98d2122d8a74
Agustin <aflouret@gmail.com>
2021-12-13 17:44:18 -0300

    concurrency

--- app/controllers/releases_controller.rb
+++ app/controllers/releases_controller.rb
@@ -1,5 +1,6 @@
 class ReleasesController < ApplicationController
     require "discogs"
+    require "thread"
 
     before_action do
         @discogs = Discogs::Wrapper.new("music-app", user_token: "SrWKXKdZFMqToajiBxyusQaZroBGcbNNiHIljyLk")
@@ -9,6 +10,23 @@ class ReleasesController < ApplicationController
         releases = Release.all
 
         @releases = releases.sort_by{|release| release.average_rating}.reverse!
+        @covers = {}
+        mutex = Mutex.new
+        threads = []
+        @releases.each do |release|
+            threads << Thread.new do
+                cover = @discogs.get_master(release.id).images[0].resource_url
+                mutex.synchronize do
+                    @covers[release.id] = cover
+                end
+            end
+        end
+        threads.map(&:join)
+
+        # @releases.each do |release|
+        #     cover = @discogs.get_master(release.id).images[0].resource_url
+        #     @covers[release.id] = cover
+        # end
     end
     
     def show
